a02eb62dddf9083063c8cfdcf11b0c7c3aa8592c
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-08 03:29:04 +0000

    Snapshot of branch master taken on 2019-04-07T23:29:03.507-04:00 from "https://github.com/UnofficialJuliaMirror/Syslogs.jl-cea106d9-e007-5e6c-ad93-58fe2094e9c4"

---
+++ test/helpers.jl
@@ -0,0 +1,41 @@
+exists(lib::Ptr, sym::Symbol) = Libdl.dlsym_e(lib, sym) != C_NULL
+
+# https://github.com/JuliaLang/julia/pull/25646/
+# The old behaviour was equivalent to the new `readuntil` called with `keep=true`, but the new
+# `readuntil` defaults to `keep=false`.
+# It's not possible for Julia to differentiate between a method that accepts kwargs and one that
+# doesn't, so a graceful deprecation is not possible.
+@static if VERSION < v"0.7.0-DEV.3510"
+    _readuntil(args...) = readuntil(args...)
+else
+    _readuntil(args...) = readuntil(args...; keep=true)
+end
+
+function udp_srv(port::Int)
+    r = Future()
+
+    sock = UDPSocket()
+    bind(sock, ip"127.0.0.1", port)
+
+    @async begin
+        put!(r, String(recv(sock)))
+        close(sock)
+    end
+
+    return r
+end
+
+function tcp_srv(port::Int)
+    r = Future()
+
+    server = listen(ip"127.0.0.1", port)
+    @async begin
+        while !isready(r)
+            sock = accept(server)
+            put!(r, _readuntil(sock, '\0'))
+        end
+        close(server)
+    end
+
+    return r
+end
